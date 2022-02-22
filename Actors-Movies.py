f2e7eb6622bb37f58656d91b33df8da9b2fba878
Dylan0888 <Dylanj55521@outlook.com>
2022-02-22 18:05:13 +0000

    added plan to plot graph data

---
+++ Actors-Movies.py
@@ -0,0 +1,7 @@
+# shows menu, asks to enter actor 
+# can present one actor or asks to disaply multple acotrs to compare to each oterh 
+# Opt 1: enter acotor ---- shows a unqiue graph to the acotr 
+# opt 2: Allows multiple acotrs to be entered to compare 
+
+# option 1 shows data in function plots 
+# option 2 shows data in multi bar plot 
