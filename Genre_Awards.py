c73e9c644377e0fe46e5af582924e96df4473431
CSMRMCU <CSMRMCMU@ljmu.ac.uk>
2022-02-23 13:20:28 +0000

    Adding genre options

---
+++ Genre_Awards.py
@@ -0,0 +1,28 @@
+#Genre options
+print("""
+    -------( Select a genre )----------- 
+    
+    \t 1. Action
+    \t 2. Adventure
+    \t 3. Animation
+    \t 4. Biography
+    \t 5. Comedy
+    \t 6. Crime
+    \t 7. Documentary 
+    \t 8. Drama
+    \t 9. Family
+    \t 10. Fantasy
+    \t 11. Film-noir 
+    \t 12. History 
+    \t 13. Horror 
+    \t 14. Music/Musical 
+    \t 15. Mystery 
+    \t 16. Romance
+    \t 17. Sci-fi 
+    \t 18. Sport 
+    \t 19. Thriller 
+    \t 20. War 
+    \t 21. Western                                 
+    -----------------------------------
+    """)
+#Asking user to select a genre
