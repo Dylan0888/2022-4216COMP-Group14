08690ed130c41304998fb5c40fb5283ca4a8916c
CSMRMCU <CSMRMCMU@ljmu.ac.uk>
2022-02-28 14:09:01 +0000

    updating genre menu

--- Genre_Awards.py
+++ Genre_Awards.py
@@ -1,28 +1,100 @@
-#Genre options
-print("""
-    -------( Select a genre )----------- 
-    
-    \t 1. Action
-    \t 2. Adventure
-    \t 3. Animation
-    \t 4. Biography
-    \t 5. Comedy
-    \t 6. Crime
-    \t 7. Documentary 
-    \t 8. Drama
-    \t 9. Family
-    \t 10. Fantasy
-    \t 11. Film-noir 
-    \t 12. History 
-    \t 13. Horror 
-    \t 14. Music/Musical 
-    \t 15. Mystery 
-    \t 16. Romance
-    \t 17. Sci-fi 
-    \t 18. Sport 
-    \t 19. Thriller 
-    \t 20. War 
-    \t 21. Western                                 
-    -----------------------------------
-    """)
-#Asking user to select a genre
+#Genre menu options
+def genreMenu():
+    print("-------( Select a genre )-----------")
+    print("[1] Action")
+    print("[2] Adventure")
+    print("[3] Animation")
+    print("[4] Biography")
+    print("[5] Comdey")
+    print("[6] Crime")
+    print("[7] Documentary")
+    print("[8] Drama")
+    print("[9] Family")
+    print("[10] Fantasy")
+    print("[11] Film-Noir")
+    print("[12] History")
+    print("[13] Horror")
+    print("[14] Music/Musical")
+    print("[15] Mystery")
+    print("[16] Romance")
+    print("[17] Sci-fi")
+    print("[18] Sport")
+    print("[19] Thriller")
+    print("[20] War")
+    print("[21] Western")
+    print("[0] Exit")
+
+#Asking user for input
+genreMenu()
+option = int(input("Enter genre choice: "))
+
+while option != 0:
+    if option == 1:
+        #Action stuff goes here
+        print("Action Genre")
+    elif option == 2:
+        #Adventure stuff goes here
+        print("Adventure genre")
+    elif option == 3:
+        #Animation stuff goes here
+        print("Animation genre")
+    elif option == 4:
+        #Biography stuff goes here
+        print("Biography genre")   
+    elif option == 5:
+        #Comdey stuff goes here
+        print("Comdey genre")
+    elif option == 6:
+        #Crime stuff goes here
+        print("Crime genre")
+    elif option == 7:
+        #Documentary stuff goes here
+        print("Documentary genre") 
+    elif option == 8:
+        #Drama stuff goes here
+        print("Drama genre")
+    elif option == 9:
+        #Family stuff goes here
+        print("Family genre")
+    elif option == 10:
+        #Fantasy stuff goes here
+        print("Fantasy genre") 
+    elif option == 11:
+        #Film-Noir stuff goes here
+        print("Film-Noir genre")
+    elif option == 12:
+        #History stuff goes here
+        print("History genre")
+    elif option == 13:
+        #Horror stuff goes here
+        print("Horror genre")   
+    elif option == 14:
+        #Music/Musical stuff goes here
+        print("Music/Musical genre")
+    elif option == 15:
+        #Mystery stuff goes here
+        print("Mystery genre")
+    elif option == 16:
+        #Romance stuff goes here
+        print("Romance genre") 
+    elif option == 17:
+        #Sci-fi stuff goes here
+        print("Sci-fi genre")
+    elif option == 18:
+        #Sport stuff goes here
+        print("Sport genre")
+    elif option == 19:
+        #Thriller stuff goes here
+        print("Thriller genre") 
+    elif option == 20:
+        #War stuff goes here
+        print("War genre")
+    elif option == 21:
+        #Western stuff goes here
+        print("Western genre") 
+    else:
+        print("Invalid option")    
+
+    print()
+    genreMenu()  
+    option = int(input("Enter genre choice: ")) 
