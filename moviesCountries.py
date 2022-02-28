c907da8b290fb4323abf28b370429cbe6992cddf
MackenzieW22 <macwhitehead@live.com>
2022-02-28 18:07:14 +0000

    Added Movies Countries FIle

---
+++ moviesCountries.py
@@ -0,0 +1,28 @@
+"""
+
+Movies - Countries (between two years)
+By Mackenzie Whitehead
+
+Main Graph Idea: 
+A heatmap of countries showing the amount of movies released per country in a certain time frame
+
+Steps:
+    - Asks lower limit year
+    - Asks upper limit year
+    - Generates Heatmap of movies per countries between the two given dates
+
+"""
+
+# Imports
+
+
+
+# Global Variables
+
+
+
+# Functions
+
+def createGraph(movieData):
+    print("[SUB-FILE] Successfully Passed Full Array into Sub File!")
+    print(movieData)
