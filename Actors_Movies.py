081ba3dd226c4058e32c56807ab20f5308c295f9
Dylan0888 <Dylanj55521@outlook.com>
2022-03-22 18:55:05 +0000

    Added code to add the rating a graph

--- Actors_Movies.py
+++ Actors_Movies.py
@@ -43,6 +43,7 @@ def singGraph(year, cast, imdbRating):
     appereanceYear = [] #Actor Appereances per year 
     actorAppearances = []
     actorList = []
+    getImdb = []
     print("Compering your choice to the records... ")
     
 
@@ -50,11 +51,10 @@ def singGraph(year, cast, imdbRating):
  
     for i in range(len(cast)):
         actorList = str(cast[i]).split(", ")
+        #getImdb = imdbRating[i]
         for j in range(len(actorList)):
             if actorList[j] == str(chosenActor):
                 print("Found Actor")
-                
-
                 if len(appereanceYear) != 0:
                     if year[i] in appereanceYear:
                         for k in range(len(appereanceYear)):
@@ -64,15 +64,18 @@ def singGraph(year, cast, imdbRating):
                     else:
                         appereanceYear.append(year[i])
                         actorAppearances.append(int(1))
+                        getImdb.append(imdbRating[i])
+                        
      
                 else:
                     appereanceYear.append(year[i])
                     actorAppearances.append(int(1))
+                   
                     
 
     print ("The acotr was found in the following: ")
     print (appereanceYear, " and ",actorAppearances)
-
+    print (getImdb)
     appereanceYear.sort()
    
 
@@ -81,7 +84,7 @@ def singGraph(year, cast, imdbRating):
     #------------Example graph data--------#
     #years = [1,2,3,4,5,6]
     #movieAppearances = [1,2,3,4,5,6] 
-    imdbRatings = [2, 3.5, 4, 9, 10.2, 11]
+    #imdbRatings = [2, 3.5, 4, 9, 10.2, 11]
 
     fig, ax = plt.subplots()
     ax.plot(appereanceYear, actorAppearances, 'go--', label="Movies") #Green Plot
@@ -91,7 +94,7 @@ def singGraph(year, cast, imdbRating):
     ax.set_title("Actor: "+ chosenActor, fontsize=14)
     ax.set_xlabel("Years", fontsize=12)
     ax.set_ylabel("Amount of movies", fontsize=12, color='g')
-    #ax.plot(appYear, imdbRatings, 'ro--', label="Rating") #Red Plot #
+    ax.plot(getImdb, 'ro--', label="Rating") #Red Plot #
 
     #---Used to add second axis lable on the right of the graph---#
     ax1 = ax.twinx()
@@ -99,7 +102,7 @@ def singGraph(year, cast, imdbRating):
 
     ax.legend()#adds axis lables on the graph
    
-    ax.grid(True) # Shows grid design for easier viewing 
+    #ax.grid(True) # Shows grid design for easier viewing 
     plt.show()
 
 def multiGraph():
