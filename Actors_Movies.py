7a72d52c0a535cd366debdf6809e1e2162baaea0
Dylan0888 <Dylanj55521@outlook.com>
2022-03-23 22:07:43 +0000

    Added functionality two duel graph option

--- Actors_Movies.py
+++ Actors_Movies.py
@@ -24,12 +24,12 @@ def menuOptions(year, cast, imdbRating):
             
         elif userChoice == "2":
             userSelection = True
-            multiGraph ()
+            multiGraph (year, cast)
         
         else:
             print("Please select a valid input!")
 
-#Loads graph for single selected actor 
+# ---- Loads graph for single selected actor ---- # 
 def singGraph(year, cast, imdbRating):
    
     # ---- Variables for the loop to store data in ---- # 
@@ -96,29 +96,96 @@ def singGraph(year, cast, imdbRating):
     ax1.grid(False)
     plt.show()
 
-def multiGraph():
-    actor1 = input("Please enter your first actor:")
-    actor2 = input("Please enter your second actor:")
-    
-    print("Loading multiple graphs....")
 
-    actorOneYears = [1,2,3,4,5,6]
-    actorTwoYears = [8,9,10,11,12,13]
-    movieAppearances = [1,2,3,4,5,6]
+# ---- Loads Graph for double actor comparison ---- # 
+def multiGraph(year, cast):
+    
+    
+    # a1 = Actor 1
+    a1AppereanceYear = []
+    a1ActorAppearances = []
+    a1ActorList = []
+    # a2 = Actor 2
+    a2AppereanceYear = []
+    a2ActorAppearances = []
+    a2ActorList = []
+
+    # ---- Loops to find the first actor in the data ---- # 
+    a1Found = False
+    while (a1Found != True):
+        a1 = input("Please enter your first actors full name: ")
+        for i in range(len(cast)):
+            a1ActorList = str(cast[i]).split(", ")
+            for j in range(len(a1ActorList)):                
+                if a1ActorList[j] == str(a1): # if actor was found it will then find all the times their name appears 
+                    a1Found = True  
+                    if len(a1AppereanceYear) != 0:
+                        if year[i] in a1AppereanceYear: # when the actor is found the dates are stored in the array 
+
+                            for k in range(len(a1AppereanceYear)):
+                                if int(a1AppereanceYear[k]) == int(year[i]):
+                                    a1ActorAppearances[k] = int(a1ActorAppearances[k]) + 1 # if the same year is present then it will add one to that year    
+                        else:
+                            a1AppereanceYear.append(year[i])
+                            a1ActorAppearances.append(int(1))
+                               
+                    else:
+                        # if the year does not exist in the array then it will add it 
+                        a1AppereanceYear.append(year[i])
+                        a1ActorAppearances.append(int(1))
+        # Validation for the user entered actor 
+        if a1Found == False:
+            print("Actor was not found, please enter another actor. \n")   
+        elif a1Found == True:
+            print("Actor Found")  
+                    
+    # ---- Users Enters second actor and scans through database ---- # 
+    a2Found = False 
+    while (a2Found != True):
+            a2 = input("Please enter your second actors full name: ")
+            for i in range(len(cast)):
+                a2ActorList = str(cast[i]).split(", ")
+                for j in range(len(a2ActorList)):                
+                    if a2ActorList[j] == str(a2): # if actor was found it will then find all the times their name appears 
+                        a2Found = True  
+                        if len(a2AppereanceYear) != 0:
+                            if year[i] in a2AppereanceYear: # when the actor is found the dates are stored in the array 
+
+                                for k in range(len(a2AppereanceYear)):
+                                    if int(a2AppereanceYear[k]) == int(year[i]):
+                                            a2ActorAppearances[k] = int(a2ActorAppearances[k]) + 1 # if the same year is present then it will add one to that year    
+                            else:
+                                a2AppereanceYear.append(year[i])
+                                a2ActorAppearances.append(int(1))
+                                    
+                        else:
+                                # if the year does not exist in the array then it will add it 
+                            a2AppereanceYear.append(year[i])
+                            a2ActorAppearances.append(int(1))
+                # Validation for the user entered actor 
+            if a2Found == False:
+                print("Actor was not found, please enter another actor. \n")   
+            elif a2Found == True:
+                print("Actor two Found")  
+                 
+
+
+    print("Loading multiple graphs....")
 
     fig, axs = plt.subplots(1,2, sharey=True)
 
     #-----Out of graph design-------#
-    axs[0].set_title(actor1, fontsize=16)
-    axs[1].set_title(actor2, fontsize=16)
-    axs[0].set_xlabel("years", fontsize=12)
-    axs[1].set_xlabel("years", fontsize=12)
+    axs[0].set_title(a1, fontsize=16)
+    axs[0].set_xlabel("years", fontsize=10)
     axs[0].set_ylabel("Total of movies", fontsize=12)
-
-    axs[0].plot(actorOneYears, movieAppearances, 'gD:')
-    axs[1].plot(actorTwoYears, movieAppearances, 'mD:')
-
+    axs[0].plot(a1AppereanceYear, a1ActorAppearances, 'gD:')
+    axs[0].tick_params(axis='x', labelrotation = 90) # Rotates the axis data to fit all on the graph 
+    
+    axs[1].set_title(a2, fontsize=16)
+    axs[1].set_xlabel("years", fontsize=10)
+    axs[1].plot(a2AppereanceYear, a2ActorAppearances, 'mD:')
+    axs[1].tick_params(axis='x', labelrotation = 90) # Rotates the axis data to fit all on the graph 
+    
+    plt.ylim([0, 10]) # sets the scale on the y axis 
     plt.tight_layout()
     plt.show()
-
-
