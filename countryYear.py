0114d562b4f3b1e5f4e27919f6d2468f8471c3f2
CSMRMCU <CSMRMCMU@ljmu.ac.uk>
2022-03-31 14:48:31 +0100

    menu able to get countries from file

--- countryYear.py
+++ countryYear.py
@@ -6,6 +6,9 @@ def menuOption(year, country):
 
     choiceSelected = False
     choiceInput = False
+    countryList = []
+    datesInvalid = True
+    
 
     while (choiceSelected == False):
         userChoice = input("""
@@ -20,110 +23,59 @@ def menuOption(year, country):
         if userChoice == "1":
             choiceSelected = True
             print("Country vs Country selected")
-            
+
             while(choiceInput == False):
                 countryOne = str(input("Select a country: "))
                 countryTwo = str(input("Select a second country: "))
 
-                if countryOne is country:
-                    print("Invalid input, please try again")
-                    choiceInput = False
+                for i in range(len(country)):
+                    countryList = str(country[i]).split(", ")
+                    for j in range(len(countryList)):
+                        if str(countryList[j]) == countryOne:  
+                            choiceInput = True
+                            if str(countryList[j]) == countryTwo:  
+                                choiceInput = True
 
-                if countryTwo is country:
-                    print("Invalid input, please try again")
-                    choiceInput = False
+                if choiceInput == False:
+                    print("Invalid input, please try again")    
+                elif choiceInput == True:
+                    choiceTwo = input("""
+                     Would you like the data between two specific dates or all time?
 
-                if countryOne and countryTwo is not country:
-                    choiceInput = True
-                
-                else:
-                    choiceInput = False
-                    print("Invalid input, please try again")  
+                    [1] Two date
+                    [2] All Time
+        
+                    --Please select a option--
+                    """)
+                    if (choiceTwo == "1"):
+                        while (datesInvalid == True):
+                            dateOne = int(input("Select the first date: "))
+                            dateTwo = int(input("Select the second date: "))
 
-            if choiceInput == True:           
-                yearMenu(year, country)
-                userChoice = True
+                            if dateTwo < dateOne:
+                                print("Second date must be more recent than the first, e.g. 1930-1940")
+                                datesInvalid == True
+                            if (dateOne >= 1874) and (dateTwo <= 2019):
+                                datesInvalid = False
+                            else: 
+                                print("dates must be between 1874 and 2019, please try again")   
+                        if datesInvalid == False:
+                            createGraphOne(year, country, dateOne, dateTwo)
 
-
+                    elif(choiceTwo == "2"):
+                        print("havent done this yet")                 
         elif userChoice == "2":
             choiceSelected = True
             print("All Countries selected")
+  
+def createGraphOne(year, country, dateOne, dateTwo):
+    print("havent done this yet")
+
+
+
+
     
-def yearMenu(year, country):
-    datesInvalid = True
-
-    userChoice2 = input("""
-    Would you like the data between two specifc dates or all time?
-
-        [1] Two dates
-        [2] All time
-        [3] Exit
-        
-        --Please select a option--
-            """)   
-
-    if userChoice2 == "1":
-        while(datesInvalid == True):
-            dateOne = int(input("Select a date: "))
-            dateTwo = int(input("Select a second date: "))
-
-            if dateTwo < dateOne:
-                print("Second date must be more recent than the first, e.g. 1930-1940")
-                datesInvalid = True
-            
-            if (dateOne >= 1874) and (dateTwo <= 2019):
-                datesInvalid = False
-                
-            
-            else:
-                datesInvalid = True 
-                print("Selected dates must be between 1874 and 2019")
-
-        if datesInvalid == False:
-            cvcGraph(year, country, dateOne, dateTwo)
-
-##IS LOADING MENU OPTION AFTER SELECTING YEARS NEED TO FIX
 
 
-def cvcGraph(year, country, dateOne, dateTwo):
     
-    countryYear = []
-    countryList = []
-    countryCount = []
-    countCont = []
-    countriesFound = False
-    selectedCountries = menuOption(year, country)
-
-    while (countriesFound == False):
-        for j in range(len(country)):
-            countryList = str(country[j]).split(", ")
-            print(country[j])
-
-            for y in range(len(countryList)):
-                if str(countryList[y]) == selectedCountries:
-                    countriesFound = True
-                    if len(countryYear) !=0:
-                        if year[j] in countryYear:
-                            print("test 1")
-
-                            for i in range(len(countryCount)):
-                                if str(countryList[i]) == str(country[j]):
-                                    countCont[i] = int(countCont[i]) + 1
-                                    print("test 2")
-                        else:
-                            countryYear.append(year[j])
-                            countCont.apped(int(1))
-
-                    else:
-                            countryYear.append(year[j])
-                            countCont.apped(int(1)) 
-
-        if (countriesFound) == True:
-         countCont.sort()
-         print("Loading graph please wait...")
-         print(countCont)
             
-
-        
-
-
