88cdfdf20315a9d7c9250ece2ed40ed51c34f695
CSMRMCU <CSMRMCMU@ljmu.ac.uk>
2022-04-04 16:27:50 +0100

    Changes to graph

--- countryYear.py
+++ countryYear.py
@@ -2,6 +2,7 @@ from unicodedata import numeric
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from collections import Counter
 
 def menuOption(year, country):
 
@@ -128,9 +129,53 @@ def createGraphTwo(year, country, countryOne, countryTwo):
     plt.show()
 
 def createGraphThree(year, country):
+    countries = ['USA', 'France', 'UK', 'Spain', 'Australia', 'Denmark', 'Russia', 'Italy', 'Sweden', 'Germany', 'Hungary', 'Soviet Union', 'Finland', 'Austria', 'Norway', 'Japan', 'Czechoslovakia', 'Netherlands', 'Portugal', 'Brazil', 'Canada', 'Mexico', 'nan', 'Switzerland', 'China', 'West Germany', 'India', 'East Germany', 'Egypt', 'Morocco', 'Poland', 'Romania', 'Greece', 'Hong Kong', 'Cuba', 'Ireland', 'Venezuela', 'Yugoslavia', 'Bulgaria', 'Philippines', 'Croatia', 'South Africa', 'Belgium', 'Argentina', 'Israel', 'Monaco', 'Algeria', 'Taiwan', 'Senegal', 'Bolivia', 'South Korea', 'Mauritania', 'Turkey', 'Liechtenstein', 'Iceland', 'Iran', 'Estonia', 'Luxembourg', 'Peru', 'Tunisia', 'Chile', 'Cameroon', 'Syria', 'Angola', 'Jamaica', 'Bangladesh', 'Panama', 'Ethiopia', 'Zimbabwe', 'Lebanon', 'Libya', 'Kuwait', "C�te d'Ivoire", 'New Zealand', 'Singapore', 'Botswana', 'Malta', 'Puerto Rico', 'Burkina Faso', 'Martinique', 'North Korea', 'Colombia', 'Albania', 'Palestine', 'Zaire', 'Mali', 'Ghana', 'Indonesia', 'Namibia', 'Vietnam', 'Papua New Guinea', 'Armenia', 'Kazakhstan', 'Uruguay', 'Federal Republic of Yugoslavia', 'Czech Republic', 'Slovenia', 'Guinea', 'Lithuania', 'Republic of Macedonia', 'Dominican Republic', 'Bosnia and Herzegovina', 'Slovakia', 'Georgia', 'Ukraine', 'Aruba', 'Cyprus', 'Tajikistan', 'Latvia', 'Kyrgyzstan', 'Uzbekistan', 'Pakistan', 'Bhutan', 'Faroe Islands', 'Nepal', 'Thailand', 'Malaysia', 'Haiti', 'Barbados', 'Gabon', 'Serbia', 'Jordan', 'Chad', 'Serbia and Montenegro', 'Kenya', 'United Arab Emirates', 'Afghanistan', 'Cambodia', 'Isle Of Man', 'Mongolia', 'Bahrain', 'Bahamas', 'Ecuador', 'Rwanda', 'Sri Lanka', 'Iraq', 'Paraguay', 'Costa Rica', 'Macao', 'Saudi Arabia', 'Qatar', 'Trinidad and Tobago', 'Liberia', 'Tanzania', 'El Salvador', 'Belarus', 'Nigeria', 'Guatemala', 'Grenada', 'The Democratic Republic Of Congo', 
+'Moldova', 'American Samoa', 'Uganda', 'Samoa', 'Somalia', 'Bermuda', 'Madagascar', 'Laos', 'Micronesia', 'Montenegro', 'Honduras', 'Nicaragua', 'Greenland', 'Congo', 'Reunion', 'Korea', 'Brunei', 'Burma', 'Swaziland', 'Kosovo', 'Suriname', 'Vanuatu']
+    countriesAwards = []
+    countCountry = 0
+    countriesAwards1 = []
+    countriesAwards2 = []
+    #print(len(countriesAwards))
+    #for i in range(len(country)): 
+    #    countryList = str(country[i]).split(", ")
+    #    for P in range(len(countryList)):
+    #        if countries[P] in countryList:
+    #            countCountry+=1
+    #        countriesAwards[P] = (countCountry)
+    #Counter(countries)
     
-    labels = countryList
-    sizes = df
+    x = Counter(country)
+    for i in range(len(countries)):
+        
+        countriesAwards.append(x[countries[i]])
+    #for i in range(len(x)):
+     #   for l in range(len(countries)):
+            #if ("'" + countries[l] + "':") == str(x[i]).split(" "):
+    
+        #if countries[P] == countryList[S]:
+        
+            #counterCount = counterCount + str(country[i]).count(countries[P])
+            
+        
+            #countriesAwards.append(countCountry)
+    #for l in range(len(countriesAwards)):
+     #   countriesAwards1 = str(countriesAwards[l]).split("True:")
+      #  for P in range(len(countriesAwards1)):
+       #     countriesAwards2 = str(countriesAwards1[P]).split("})")
+    #print(countriesAwards)
+    #countriesAwards2 = str(countriesAwards1[3])
+    
+    #print(countries, countriesAwards)
+    #label = ""
+    #for L in range(len(countries)):
+     #   label = label + "," + countries[L]
+
+    #label2 = ""
+    #for L in range(len(countriesAwards)):
+     #   label2 = label2 + "," + countriesAwards[L]
+    
+    labels = list(countries)
+    sizes = list(countriesAwards)
     explode = (0.1,0)
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, explode = None, labels = labels, autopct = '%1.1f%%', shadow = True, startangle = 90)
